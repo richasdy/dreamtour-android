ad182b1cb22658c17de6fbf924068d15b2762bff
richasdy <donni.richasdy@gmail.com>
2018-10-19 09:48:03 +0700

    add webview

--- app/src/main/java/ml/luciddream/dreamtour/MainActivity.java
+++ app/src/main/java/ml/luciddream/dreamtour/MainActivity.java
@@ -2,12 +2,22 @@ package ml.luciddream.dreamtour;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private WebView webView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+//        setContentView(R.layout.activity_main);
+
+        setContentView(R.layout.webview);
+        webView = (WebView) findViewById(R.id.webView1);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.loadUrl("http://itb.dreamtour.ml");
+
     }
 }
